f6d016222a2c50d2f486de4691763fa35de4d163
zmh123mh <136402607+zmh123mh@users.noreply.github.com>
2023-08-03 10:40:25 +0800

    Delete unpackage/dist/dev/mp-weixin directory

--- unpackage/dist/dev/mp-weixin/subPages/proDetails/proDetails.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/web/作业/2023.7.25轻醒/电商页面/电商小程序/subPages/proDetails/proDetails.vue"]]);
-wx.createPage(MiniProgramPage);
